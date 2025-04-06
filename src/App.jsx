8e2ea71272433ff88eb9cca2b22114b80fcc6f64
Franco Giacobbe <frandg47@gmail.com>
2025-04-06 17:22:17 -0300

    creando tarjetas de skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from './components/About'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Skill from './components/Skill'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <About/>
+        <About />
+        <Skill />
       </main>
     </>
   )
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import SkillCard from './SkillCard';
+
+const skillItem = [
+    {
+      imgSrc: './public/images/c#.jpg',
+      label: 'C#',
+      desc: 'Programación Orientada a Objetos'
+    },
+    {
+      imgSrc: './public/images/sqlserver.png',
+      label: 'SQL Server',
+      desc: 'Base de datos relacional'
+    },
+    {
+      imgSrc: './public/images/mongo.png',
+      label: 'MongoDB',
+      desc: 'Base de datos NoSQL'
+    },
+    {
+      imgSrc: './public/images/express.png',
+      label: 'ExpressJS',
+      desc: 'Framework para Node.js'
+    },
+    {
+      imgSrc: './public/images/react.png',
+      label: 'React',
+      desc: 'Librería frontend'
+    },
+    {
+      imgSrc: './public/images/node.jpg',
+      label: 'NodeJS',
+      desc: 'Entorno de ejecución'
+    }
+  ];
+  
+
 
 const Skill = () => {
   return (
-    <div>Skill</div>
+    <section className="section">
+        <div className="container">
+
+            <h2 className="headline-2">
+                Herramientas Principales que utilizo
+            </h2>
+
+            <p className="text-zinc-400 mt-3 mb-8 max-w[50ch]">
+                Descubre las poderosas herramientas y tecnologias que utilizo para crear aplicaciones & sitios web de alta calidad
+            </p>
+
+            <div className="">
+                {
+                    skillItem.map(({ imgSrc, label, desc }, key) => (
+                        <SkillCard />
+                    )
+                )
+                }
+            </div>
+
+        </div>
+
+    </section>
   )
 }
 
