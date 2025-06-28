3e3887c23dac2d1882c7fe420efe1f1bb6afb433
Franco Giacobbe <frandg47@gmail.com>
2025-06-28 17:11:11 -0300

    agregando nuevos proyectos a mi portfolio

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,47 +1,53 @@
-import React from 'react'
-import ProjectCard from './ProjectCard';
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 const works = [
-    {
-        imgSrc: '../public/projectAppHotel.png',
-        title: 'Full stack hotel app',
-        tags: ['API', 'Scrum', 'Development'],
-        projectLink: 'https://rolling-hotel.netlify.app/'
-    },
-    {
-        imgSrc: '../public/projectSystemGym.png',
-        title: 'Sistema de gestion gym',
-        tags: ['Capas', 'POO', 'SQL'],
-        projectLink: 'https://github.com/frandg47'
-    }
+  {
+    imgSrc: "/projectGioArqui.png",
+    title: "Gio Arqui | Diseño & Construcción",
+    tags: ["ReactVite", "MongoDB", "Express"],
+    projectLink: "https://gioarqui.com",
+  },
+  {
+    imgSrc: "",
+    title: "Delivery Flash App",
+    tags: ["API", "SQL", ""],
+    projectLink: "https://github.com/frandg47",
+  },
+  {
+    imgSrc: "../public/projectAppHotel.png",
+    title: "Full stack hotel app",
+    tags: ["API", "Scrum", "Development"],
+    projectLink: "https://rolling-hotel.netlify.app/",
+  },
+  {
+    imgSrc: "../public/projectSystemGym.png",
+    title: "Sistema de gestion gym",
+    tags: ["Capas", "POO", "SQL"],
+    projectLink: "https://github.com/frandg47",
+  },
 ];
 
 const Work = () => {
-    return (
-        <section
-        id="work"
-        className="section"
-        >
-        <div className="container">
+  return (
+    <section id="work" className="section">
+      <div className="container">
+        <h2 className="headline-2 mb-8">Mis proyectos personales</h2>
 
-            <h2 className="headline-2 mb-8">
-                Mis proyectos personales
-            </h2>
-
-            <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-                {works.map(({ imgSrc, title, tags, projectLink }, key) => (
-                    <ProjectCard 
-                    key={key}
-                    imgSrc={imgSrc}
-                    title={title}
-                    tags={tags}
-                    projectLink={projectLink}
-                    />
-                ))}
-            </div>
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
         </div>
-        </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Work
+export default Work;
